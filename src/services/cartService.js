6fe2c4ed79ecb53adf08288bcdabbed05d3c07ef
KONANGI GOPI <gopikonangi0001@gmail.com>
2025-09-20 10:47:27 +0530

    Update cartService.js

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const API_URL = "http://54.85.143.179:8081/api/cart";
+const API_URL = "http://54.145.53.58:8081/api/cart";
 
-const API_BASE = "http://54.85.143.179:8081/api/cart";
+const API_BASE = "http://54.145.53.58:8081/api/cart";
 
 export const addToCart = async (userId, productId) => {
   return await axios.post(`${API_BASE}/add`, {
