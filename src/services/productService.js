761ed47e24fd55c53c3279f1aacec4c5180ef28d
KONANGI GOPI <gopikonangi0001@gmail.com>
2025-09-24 10:18:18 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://54.145.53.58:8081/api/products";  // Backend API URL
+const API_BASE_URL = "http://18.215.175.171:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
